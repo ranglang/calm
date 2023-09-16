fb44f8525b3e50f58cd23d8b9293090e963d710b
ranglang <rd@shangche.tech>
2023-09-16 16:25:27 +0800

    fix: fix soemthign

--- jep/src/e/ReflectRIEntry.scala
+++ jep/src/e/ReflectRIEntry.scala
@@ -16,47 +16,66 @@ object ReflectRIEntry extends IOApp {
           AnsiConsole.systemInstall()
         }
       }
+      i0 <- {
+        LineIO.prompt(
+          """
+            | Broaden hirozan
+            |""".stripMargin,
+          required = true
+        )
+      }
       repeatInfo <- {
         LineIO.prompt(
           """
             |Make a Plan 制定计划(抓住主要矛盾) \nCreate Milestones 分解目标（新闻发布会)）\nActually Make Time for Your Goals (日程表)  切实为你的目标腾出时间\nOnly worry about the present moment 只关注当下\nStop negotiating with yourself 不要和自己协商，尽情去完成\nRevisit Your Goals often 经常回顾你的目标,想象你完成目标的感受，真正激励自己\nForgive Your Failures（And then keep Going）原谅你的失败（然后继续前进）\nDo it anyway 无论如何都要去做，Just do it
             |            |
-            |""".stripMargin, required = true)
+            |""".stripMargin,
+          required = true
+        )
       }
       i1 <- {
         LineIO.prompt(
           """
             |Create 超转 的方法
-            |""".stripMargin, required = true)
+            |""".stripMargin,
+          required = true
+        )
       }
-        i2 <- {
-          LineIO.prompt(
-            """
+      i2 <- {
+        LineIO.prompt(
+          """
               | 沟通 事实, 自我表达，关系，诉求维度
               |
-              |""".stripMargin, required = true)
-        }
+              |""".stripMargin,
+          required = true
+        )
+      }
 
       i3 <- {
         LineIO.prompt(
           """
             |构建个人形象 (丢弃内容)
-            |""".stripMargin, required = true)
+            |""".stripMargin,
+          required = true
+        )
       }
 
       i4 <- {
         LineIO.prompt(
           """
             |Breakout 突破自己
-            |""".stripMargin, required = true)
+            |""".stripMargin,
+          required = true
+        )
       }
       _ <- {
         val resultMsg = List(
           repeatInfo,
+          i0,
           i1,
-            i2,
-            i3,
-          i4,
+          i2,
+          i3,
+          i4
         ).mkString("\n")
         Clipboard.text(resultMsg)
       }
